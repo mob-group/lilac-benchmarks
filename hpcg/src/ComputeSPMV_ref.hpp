8237dfa0a51b1e5ba4d11bf2adcb1b0dea3bd032
Bruce Collie <bruce.collie@ed.ac.uk>
2018-02-05 13:36:10 +0000

    Accelerate hpcg

---
+++ hpcg/src/ComputeSPMV_ref.hpp
@@ -0,0 +1,25 @@
+
+//@HEADER
+// ***************************************************
+//
+// HPCG: High Performance Conjugate Gradient Benchmark
+//
+// Contact:
+// Michael A. Heroux ( maherou@sandia.gov)
+// Jack Dongarra     (dongarra@eecs.utk.edu)
+// Piotr Luszczek    (luszczek@eecs.utk.edu)
+//
+// ***************************************************
+//@HEADER
+
+#ifndef COMPUTESPMV_REF_HPP
+#define COMPUTESPMV_REF_HPP
+#include "Vector.hpp"
+#include "SparseMatrix.hpp"
+
+#include <mkl.h>
+
+int ComputeSPMV_ref( const SparseMatrix & A, Vector  & x, Vector & y);
+int ComputeSPMV_mkl( const SparseMatrix & A, Vector  & x, Vector & y);
+
+#endif  // COMPUTESPMV_REF_HPP
