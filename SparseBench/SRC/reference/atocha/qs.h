3c6253760210477d2b68922c31c70cddefc97182
Bruce Collie <bruce.collie@ed.ac.uk>
2018-02-05 13:43:34 +0000

    Fix sparsebench

--- SparseBench/SRC/reference/atocha/qs.h
+++
@@ -1,4 +0,0 @@
-extern int qsi(int *jdata,int len);
-extern int qsip(int *jdata,int *jperm,int len);
-extern int qsir(int *jdata,double *rdata,int len);
-extern int qsijr(int *idata,int *jdata,double *rdata,int len);
