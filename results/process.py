0d36389d955885d0cfe5a7f7ef4c2192b88a1636
Bruce Collie <brucecollie82@gmail.com>
2018-03-15 12:27:33 +0000

    Add stats processing script

---
+++ results/process.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import csv
+import matplotlib.pyplot as plt
+import statistics
+import sys
+
+def row_to_stats(row):
+    new_row = []
+    new_row.append(row[0])
+    parsed = list(map(float, row[1:]))
+    new_row.append(len(parsed))
+    new_row.append(statistics.mean(parsed))
+    new_row.append(statistics.stdev(parsed))
+    return new_row
+
+in_file = sys.argv[1]
+with open(in_file, "r") as csvfile:
+    reader = csv.reader(csvfile)
+    writer = csv.writer(sys.stdout)
+    writer.writerow(['threads', 'samples', 'time', 'stdev'])
+    for row in reader:
+        writer.writerow(row_to_stats(row))
