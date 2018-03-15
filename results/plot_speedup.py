a3d973973eefab9c31f77a05885c4db6688070b2
Bruce Collie <brucecollie82@gmail.com>
2018-03-15 14:22:55 +0000

    Add axis

--- results/plot_speedup.py
+++ results/plot_speedup.py
@@ -4,17 +4,26 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import sys
 
+def label(fn):
+    return {
+        "npb" : "NAS-CG"
+    }[fn.split("_")[0]]
+
 def plot_data(filename, ax):
     frame = pd.read_csv(filename)
-    ax.errorbar(frame['threads'], frame['speedup'], yerr=frame['stdev'],
-            capsize=2, linewidth=1, ls=':')
+    ax.errorbar(frame['threads'][1::2], frame['speedup'][1::2],
+            yerr=frame['stdev'][1::2], capsize=2, linewidth=1, ls=':')
+    ax.set_xticks(frame['threads'][1::2])
 
 if __name__ == "__main__":
     files = sys.argv[1:]
     fig = plt.figure()
     ax = fig.add_subplot(111)
+    legend = []
     for f in files:
         plot_data(f, ax)
+        legend.append(label(f))
     ax.set_xlabel("No. of MKL Threads")
     ax.set_ylabel("Speedup")
+    ax.legend(legend)
     plt.show()
