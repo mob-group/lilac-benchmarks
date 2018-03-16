79a1765888588ddeb14b7197a55c242f0f7f56f3
Bruce Collie <brucecollie82@gmail.com>
2018-03-16 17:13:00 +0000

    Add sparsebench

--- results/plot_speedup.py
+++ results/plot_speedup.py
@@ -8,13 +8,15 @@ def label(fn):
     return {
         "npb" : "NAS-CG",
         "pfold" : "Wales-PFold",
+        "sparsebench" : "Netlib-CRS",
     }[fn.split("_")[0]]
 
 def plot_data(filename, ax):
+    s = slice(1,None,1)
     frame = pd.read_csv(filename)
-    ax.errorbar(frame['threads'][1::2], frame['speedup'][1::2],
-            yerr=frame['stdev'][1::2], capsize=2, linewidth=1, ls=':')
-    ax.set_xticks(frame['threads'][1::2])
+    ax.errorbar(frame['threads'][s], frame['speedup'][s],
+            yerr=frame['stdev'][s], capsize=2, linewidth=1, ls=':')
+    ax.set_xticks(frame['threads'][s])
 
 if __name__ == "__main__":
     files = sys.argv[1:]
