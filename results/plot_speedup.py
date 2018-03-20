a8e40102cf4a76184fe01dbe376aa6d248a6fc35
Bruce Collie <brucecollie82@gmail.com>
2018-03-20 10:48:36 +0000

    Make nicer plot

--- results/plot_speedup.py
+++ results/plot_speedup.py
@@ -8,18 +8,19 @@ def label(fn):
     return {
         "npb" : "NAS-CG",
         "pfold" : "Wales-PFold",
+        "ngt" : "Wales-NGT",
         "netlib" : "Netlib-CRS",
         "cnetlib" : "Netlib-C-CRS",
-        "ngt" : "Wales-NGT"
     }[fn.split("_")[0]]
 
 def plot_data(filename, ax):
-    s = slice(0,None,1)
+    s = slice(1,16,1)
     frame = pd.read_csv(filename)
     # ax.errorbar(frame['threads'][s], frame['speedup'][s],
     #         yerr=frame['stdev'][s], capsize=2, linewidth=1, ls=':')
     ax.plot(frame['threads'][s], frame['speedup'][s], linewidth=1, ls='-')
     ax.set_xticks(frame['threads'][s])
+    return "{:.2f}".format(frame['speedup'][7])
 
 if __name__ == "__main__":
     files = sys.argv[1:]
@@ -27,10 +28,11 @@ if __name__ == "__main__":
     ax = fig.add_subplot(111)
     legend = []
     for f in files:
-        plot_data(f, ax)
-        legend.append(label(f))
-    ax.legend(legend)
+        speedup = plot_data(f, ax)
+        legend.append(label(f) + " (" + speedup + "×)")
+    ax.legend(legend, fontsize='small')
     ax.axhline(y=1,lw=1,ls=':')
+    ax.axvline(x=8,lw=1,ls=':')
     ax.set_xlabel("No. of MKL Threads")
     ax.set_ylabel("Speedup")
-    plt.show()
+    plt.savefig('speedup.pdf', papertype='a4')
