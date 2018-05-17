4ed47f6470cd3b6cf2c3ec151a1e322148a73871
Bruce <brucecollie82@gmail.com>
2018-05-17 12:12:58 +0100

    Fewer copies

--- NPB3.3.1/NPB3.3-ACCEL/CG/gpu.c
+++ NPB3.3.1/NPB3.3-ACCEL/CG/gpu.c
@@ -4,6 +4,8 @@
 
 #include <assert.h>
 #include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
 
 static cublasHandle_t cublasH = NULL;
 static cusparseHandle_t cusparseH = NULL;
@@ -19,11 +21,14 @@ static cudaError_t cudaStat3 = cudaSuccess;
 static cudaError_t cudaStat4 = cudaSuccess;
 static cudaError_t cudaStat5 = cudaSuccess;
 
-static int *d_csrRowPtrA = NULL;
-static int *d_csrColIndA = NULL;
 static double *d_csrValA = NULL;
 static double *d_x = NULL;
 static double *d_y = NULL;
+
+static int *d_index_ptr = NULL;
+static int *h_index_ptr = NULL;
+static double *d_data_ptr = NULL;
+static double *h_data_ptr = NULL;
 
 static double h_one = 1.0;
 static double h_zero = 0.0;
@@ -62,17 +67,18 @@ void setup(int n, int nnzA)
   }
 
   if(n != last_n || nnzA != last_nnzA) {
-    cudaStat1 = cudaMalloc ((void**)&d_csrRowPtrA, sizeof(int) * (n+1) );
-    cudaStat2 = cudaMalloc ((void**)&d_csrColIndA, sizeof(int) * nnzA );
-    cudaStat3 = cudaMalloc ((void**)&d_csrValA   , sizeof(double) * nnzA );
-    cudaStat4 = cudaMalloc ((void**)&d_x         , sizeof(double) * n );
-    cudaStat5 = cudaMalloc ((void**)&d_y         , sizeof(double) * n );
+    cudaStat1 = cudaMalloc ((void**)&d_index_ptr , sizeof(int) * (n + 1 + nnzA));
+    h_index_ptr = malloc(sizeof(int) * (n + 1 + nnzA));
+
+    cudaStat2 = cudaMalloc ((void**)&d_data_ptr, sizeof(double) * (nnzA + n));
+    h_data_ptr = malloc(sizeof(double) * (nnzA + n));
+
+    cudaStat3 = cudaMalloc ((void**)&d_y, sizeof(double) * n);
 
     assert(cudaSuccess == cudaStat1);
     assert(cudaSuccess == cudaStat2);
     assert(cudaSuccess == cudaStat3);
-    assert(cudaSuccess == cudaStat4);
-    assert(cudaSuccess == cudaStat5);
+    assert(h_index_ptr && h_data_ptr);
 
     last_n = n;
     last_nnzA = nnzA;
@@ -87,16 +93,17 @@ void* spmv_harness_(double* ov, double* a, double* iv, int* rowstr, int* colidx,
   setup(n, nnzA);
 
   // Do device copy
-  cudaStat1 = cudaMemcpy(d_csrRowPtrA, rowstr, sizeof(int) * (n+1)   , cudaMemcpyHostToDevice);
-  cudaStat2 = cudaMemcpy(d_csrColIndA, colidx, sizeof(int) * nnzA    , cudaMemcpyHostToDevice);
-  cudaStat3 = cudaMemcpy(d_csrValA   , a   , sizeof(double) * nnzA , cudaMemcpyHostToDevice);
+  memcpy(h_index_ptr, rowstr, sizeof(int) * (n+1));
+  memcpy(h_index_ptr + n+1, colidx, sizeof(int) * nnzA);
+  cudaStat1 = cudaMemcpy(d_index_ptr, h_index_ptr, sizeof(int) * (n + 1 + nnzA), cudaMemcpyHostToDevice);
+
+  memcpy(h_data_ptr, a, sizeof(double) * (nnzA));
+  memcpy(h_data_ptr + nnzA, iv, sizeof(double) * n);
+  cudaStat2 = cudaMemcpy(d_data_ptr, h_data_ptr, sizeof(double) * (nnzA + n), cudaMemcpyHostToDevice);
+
   assert(cudaSuccess == cudaStat1);
   assert(cudaSuccess == cudaStat2);
-  assert(cudaSuccess == cudaStat3);
   
-  cudaStat1 = cudaMemcpy(d_x, iv, sizeof(double) * n, cudaMemcpyHostToDevice);
-  assert(cudaSuccess == cudaStat1);
-
   // Do the SPMV
   cusparseStat = cusparseDcsrmv_mp(cusparseH,
                                    CUSPARSE_OPERATION_NON_TRANSPOSE,
@@ -105,10 +112,10 @@ void* spmv_harness_(double* ov, double* a, double* iv, int* rowstr, int* colidx,
                                    nnzA,
                                    &h_one,
                                    descrA,
-                                   d_csrValA,
-                                   d_csrRowPtrA,
-                                   d_csrColIndA,
-                                   d_x,
+                                   d_data_ptr,
+                                   d_index_ptr,
+                                   d_index_ptr + (n+1),
+                                   d_data_ptr + (nnzA),
                                    &h_zero,
                                    d_y);
   assert(CUSPARSE_STATUS_SUCCESS == cusparseStat);
