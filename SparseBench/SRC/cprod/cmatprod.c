972cbf04b5e1c05928af646acc515cec78ce8878
Bruce <brucecollie82@gmail.com>
2018-05-22 16:22:22 +0100

    Add c product version

--- SparseBench/SRC/cprod/cmatprod.c
+++ SparseBench/SRC/cprod/cmatprod.c
@@ -6,33 +6,15 @@
 double starttimer_(void);
 double stoptimer_(void);
 void add_mult_time_(double*);
+void* spmv_harness_(double* ov, double* a, double* iv, int* rowstr, int* colidx, int* rows);
 
 int random_crs_matprod_ (
     double *v,int *idx,int *nnz,int *ii,
     double *x,double *y,int *size)
 {
-  char *version = getenv("SPMV_VERSION");
   double start = starttimer_();
 
-  if(version && strcmp(version, "MKL") == 0) {
-    mkl_dcsrgemv("N", size, v, ii, idx, x, y);
-  } else {
-    double sum;
-    int m = *size,n = *size, shift = -1,i,j,jrow;
-
-    x    = x + shift;    /* shift for Fortran start by 1 indexing */
-    v    += shift; /* shift for Fortran start by 1 indexing */
-    idx  += shift;
-    for (i=0; i<m; i++) {
-      jrow = ii[i];
-      n    = ii[i+1] - jrow;
-      sum  = 0.0;
-      for (j=0; j<n; j++) {
-        sum += v[jrow]*x[idx[jrow]]; jrow++;
-       }
-      y[i] = sum;
-    }
-  }
+  spmv_harness_(y, v, x, ii, idx, size);
 
   double stop = stoptimer_() - start;
   add_mult_time_(&stop);
