f332009036b295afeffbc1a7e7c07c70dddc80d3
Bruce Collie <brucecollie82@gmail.com>
2018-06-25 10:05:58 +0100

    Better data manipulation

--- results/suite.py
+++ results/suite.py
@@ -5,21 +5,15 @@ import numpy as np
 import pandas as pd
 import sys
 
-def all_matrices(frame):
-    return set(frame.matrix)
-
 def speedup(sub_f):
     base_time = sub_f.query('method == "native"').time
-    speedups = {}
-    for _, row in sub_f.iterrows():
-        speedups[row.method] = float(base_time / row.time)
-        speedups['nnz'] = row.nnz
-    return speedups
+    return sub_f.assign(
+        density=lambda x: x.nnz / (x.rows * x.cols),
+        speedup=lambda x: float(base_time) / x.time
+    )
 
 if __name__ == "__main__":
     frame = pd.read_csv(sys.argv[1], sep=' ')
-    results = {}
-    for name in all_matrices(frame):
-        sub_f = frame.query('matrix == @name')
-        results[name] = speedup(sub_f)
-    data = pd.DataFrame(results).T
+    results = pd.concat([speedup(sub_f) for _, sub_f in frame.groupby(by='matrix')])
+    gpu = results.query('method == "gpu"')
+    integrated = results.query('method == "integrated"')
