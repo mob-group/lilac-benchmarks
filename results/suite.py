c5cba23c1bb3fc7ab10525f57c088cf7585fa5a4
Bruce Collie <brucecollie82@gmail.com>
2018-06-28 10:06:33 +0100

    Export C model

--- results/suite.py
+++ results/suite.py
@@ -5,23 +5,36 @@ import numpy as np
 import pandas as pd
 import sys
 
-from sklearn import linear_model
+from sklearn_porter import Porter
 
-def speedup(sub_f):
-    base_time = sub_f.query('method == "native"').time
-    return sub_f.assign(
-        density=lambda x: x.nnz / (x.rows * x.cols),
-        speedup=lambda x: float(base_time) / x.time
-    )
+from sklearn import svm
+
+def name_to_output(name):
+    return {
+        "native" : 0,
+        "opencl" : 1,
+        "clgpu" : 2
+    }[name]
+
+def output_to_name(output):
+    return ["native", "opencl", "clgpu"][output]
+
+def train_data(sub_f):
+    nat = sub_f.query('method == "native"')
+    out = sub_f.ix[sub_f.time.idxmin()].method
+    return (int(nat.rows), int(nat.nnz)), name_to_output(out)
 
 if __name__ == "__main__":
     frame = pd.read_csv(sys.argv[1], sep=' ')
-    results = pd.concat([speedup(sub_f) for _, sub_f in frame.groupby(by='matrix')])
-    gpu = results.query('method == "gpu"')
-    integrated = results.query('method == "integrated"')
-    reg = linear_model.LinearRegression()
-    reg.fit(np.array(gpu.nnz).reshape(-1,1), gpu.speedup)
-    ys = reg.coef_ * gpu.nnz + reg.intercept_
-    plt.plot(gpu.nnz, ys)
-    plt.scatter(gpu.nnz, gpu.speedup)
-    plt.show()
+    data = [train_data(sub_f) for _, sub_f in frame.groupby(by='matrix')]
+
+    train_x = [d[0] for d in data]
+    train_y = [d[1] for d in data]
+
+    clf = svm.SVC(gamma=0.001)
+    clf.fit(train_x, train_y)
+    
+    preds = clf.predict(train_x)
+
+    p = Porter(clf, language='C')
+    print(p.export(embed_data=True))
