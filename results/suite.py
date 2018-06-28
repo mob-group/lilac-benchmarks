b108835284464957eb81cd58d2035c15418e4c0c
Bruce Collie <brucecollie82@gmail.com>
2018-06-28 14:52:01 +0100

    Plot contours

--- results/suite.py
+++ results/suite.py
@@ -9,6 +9,9 @@ import sys
 from sklearn import svm
 from sklearn_porter import Porter
 
+PLOT_NNZ_MAX=250000
+PLOT_ROW_MAX=50000
+
 def name_to_output(name):
     return {
         "native" : 0,
@@ -32,10 +35,36 @@ def get_data(frame):
 
     return xs, ys
 
+def grid(features):
+    rows, nnz = [np.array(l) for l in zip(*features)]
+    r_min, r_max = rows.min(), PLOT_ROW_MAX
+    n_min, n_max = nnz.min(), PLOT_NNZ_MAX
+    xx, yy = np.meshgrid(np.arange(n_min, n_max, 100),
+                         np.arange(r_min, r_max, 100))
+    return xx, yy
+
+def plot_contours(ax, clf, xx, yy, **params):
+    """Plot the decision boundaries for a classifier.
+
+    Parameters
+    ----------
+    ax: matplotlib axes object
+    clf: a classifier
+    xx: meshgrid ndarray
+    yy: meshgrid ndarray
+    params: dictionary of params to pass to contourf, optional
+    """
+    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    out = ax.contourf(xx, yy, Z, **params)
+    return out
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('train', type=str, help='training data file')
-    parser.add_argument('--test', type=str, help='optional test data to validate model')
+    parser.add_argument('--test', type=str, help='test data file to validate model')
+    parser.add_argument('--code', action='store_true', help='generate C code for model')
+    parser.add_argument('--plot', action='store_true', help='plot trained model decision contour')
     args = parser.parse_args()
 
     frame = pd.read_csv(args.train, sep=' ')
@@ -44,6 +73,20 @@ if __name__ == "__main__":
     clf = svm.SVC(kernel='linear', gamma=0.001)
     clf.fit(train_x, train_y)
 
+    if args.plot:
+        fig, ax = plt.subplots()
+        xx, yy = grid(train_x)
+        plot_contours(ax, clf, xx, yy, cmap=plt.cm.coolwarm, alpha=0.8)
+        
+        rows, nnz = [np.array(l) for l in zip(*train_x)]
+        ax.set_xlim(xx.min(), PLOT_NNZ_MAX)
+        ax.set_ylim(yy.min(), PLOT_ROW_MAX)
+        ax.scatter(nnz, rows, c=train_y, cmap=plt.cm.coolwarm, s=20, edgecolors='k')
+
+        ax.set_xlabel("Non-zero elements")
+        ax.set_ylabel("Rows")
+        plt.show()
+
     if args.test is not None:
         test_frame = pd.read_csv(args.test, sep=' ')
         test_x, test_y = get_data(test_frame)
@@ -51,6 +94,7 @@ if __name__ == "__main__":
         results = (np.array(preds) == np.array(test_y))
         acc = 100 * sum(results) / len(results)
         print(f"Test Accuracy: {acc:.3f}%")
-    else:
+
+    if args.code:
         p = Porter(clf, language='C')
         print(p.export(embed_data=True))
