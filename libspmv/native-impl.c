23a2da2df14fdb546fb888dcb39b44c6ef017e35
Bruce Collie <brucecollie82@gmail.com>
2018-06-28 12:29:18 +0100

    Separate native implementation

---
+++ libspmv/native-impl.c
@@ -0,0 +1,12 @@
+void* native_spmv(double* ov, double* a, double* iv, int* rowstr, int* colidx, int* rows)
+{
+  int i, j;
+  for(i = 0; i < *rows; i++)
+  {
+      double value = 0.0;
+      for(j = rowstr[i] - 1; j < rowstr[i+1] - 1; j++)
+          value += a[j] * iv[colidx[j] - 1];
+      ov[i] = value;
+  }
+  return 0;
+}
---
+++ libspmv/native-impl.h
@@ -0,0 +1,6 @@
+#ifndef NATIVE_IMPL_H
+#define NATIVE_IMPL_H
+
+void* native_spmv(double* ov, double* a, double* iv, int* rowstr, int* colidx, int* rows);
+
+#endif
