ce81f723346c7fb33b1d658951fc54c8a0a0aca0
Bruce Collie <brucecollie82@gmail.com>
2018-08-31 13:38:07 +0100

    Hatch properly

--- results/cgo2.py
+++ results/cgo2.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python
 
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
+import pylab
+
+plt.rcParams['hatch.color'] = '#FFFFFF'
+plt.rcParams['hatch.linewidth'] = 1
 
 NAMES = {
     'nas': 'NAS-CG',
     'netlib': 'Netlib',
     'netlib-c': 'Netlib-C'
 }
+
+HATCHES = [
+    '////', 'xxxx', '\\\\\\\\', '....'
+]
+
+def colors(n):
+    return pylab.get_cmap('Greys')(np.linspace(0, 0.5, n))
 
 def load_data(fn):
     frame = pd.read_csv(fn)
@@ -20,16 +32,23 @@ def load_data(fn):
 def title_for(data):
     return "{} {}".format(NAMES[data.benchmark], data['size'].upper())
 
+def make_bars(data, ax):
+    cs = colors(4)
+    vals = (data.mklspeed, data.gpuspeed, data.clspeed, data.clgpuspeed)
+    for i, val in enumerate(vals):
+        ax.bar(i, val, color=cs[i], edgecolor='black', hatch=HATCHES[i])
+    ax.axhline(1, linestyle=':', color='black')
+    ax.set_title(title_for(data))
+    ax.set_xticks([])
+
 if __name__ == "__main__":
     frame = load_data('cgo_data.csv')
     fig, axes = plt.subplots(3, 6)
+    fig.set_figheight(4)
+    fig.set_figwidth(7)
     for group, row in zip(frame.groupby('benchmark'), axes):
         for (_, data), ax in zip(group[1].iterrows(), row):
-            ax.bar(0, data.mklspeed)
-            ax.bar(1, data.gpuspeed)
-            ax.bar(2, data.clspeed)
-            ax.bar(3, data.clgpuspeed)
-            ax.axhline(1, linestyle=':', color='black')
-            ax.set_title(title_for(data))
+            make_bars(data, ax)
     fig.tight_layout()
-    plt.savefig('draft.pdf', figsize=(5,7), dpi=120)
+    plt.show()
+    # plt.savefig('draft.pdf', dpi=120)
