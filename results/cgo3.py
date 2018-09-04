5d33c31cabb08b8af545537a898b2a47d13faf97
Bruce Collie <brucecollie82@gmail.com>
2018-09-04 16:19:30 +0100

    Start to work out plotting

--- results/cgo3.py
+++ results/cgo3.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python
+
+import csv
+import matplotlib.pyplot as plt
+import sys
+
+# Merge data by benchmark, then for each pair plot a pair of bars with opposing
+# styles
+# add label below
+
+if __name__ == "__main__":
+    with open(sys.argv[1]) as csvfile:
+        reader = csv.DictReader(csvfile)
