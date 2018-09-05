0b30666dcb0fdb0be8381bc6923f9db77a5a668f
Bruce Collie <brucecollie82@gmail.com>
2018-09-05 09:39:23 +0100

    Merge data

--- results/cgo3.py
+++ results/cgo3.py
@@ -8,6 +8,16 @@ import sys
 # styles
 # add label below
 
+def merged_data(data):
+    ret = {}
+    for row in data:
+        if row['name'] not in ret:
+            ret[row['name']] = {}
+        ret[row['name']][row['platform']] = row['speedup']
+    return ret
+
 if __name__ == "__main__":
     with open(sys.argv[1]) as csvfile:
         reader = csv.DictReader(csvfile)
+        data = merged_data(reader)
+    print(data)
