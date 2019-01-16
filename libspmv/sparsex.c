efce93d5e49887e67672c41d0a70b6cbf5bd765f
Bruce Collie <brucecollie82@gmail.com>
2019-01-16 11:56:53 +0000

    Add empty sparsex implementation

---
+++ libspmv/sparsex.c
@@ -0,0 +1,11 @@
+#include <sparsex/sparsex.h>
+
+void* spmv_harness_(double* ov, double* a, double* iv, int* rowstr, int* colidx, int* rows)
+{
+  return NULL;
+}
+
+void* f_spmv_harness_(float* ov, float* a, float* iv, int* rowstr, int* colidx, int* rows)
+{
+  return NULL;
+}
