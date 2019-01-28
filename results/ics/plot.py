820aa22005a96b803d4af0031153f614fb0a4075
Bruce Collie <brucecollie82@gmail.com>
2019-01-28 14:30:41 +0000

    Better vs_baseline plots

--- results/ics/plot.py
+++ results/ics/plot.py
@@ -3,9 +3,112 @@
 import argparse
 import matplotlib.pyplot as plt
 import pandas as pd
+import seaborn as sns
 
-def best_per_benchmark():
-    pass
+sns.set_style('ticks')
+sns.set_palette(sns.cubehelix_palette(3, rot=60/360, dark=0.2, light=0.7))
+
+# Compute the size of a figure based on the current columnwidth
+def fig_size(w, h):
+    colwidth = 240.94499
+    colwidth_in = colwidth / 72.26999
+    return (w * colwidth_in, h * colwidth_in)
+
+# Turn an internal representation of an implementation name into a friendly
+# printable one.
+def impl_map(platform, impl):
+    if platform == 'michel' and impl == 'opencl10':
+        return 'clSPARSE (eGPU)'
+    elif platform == 'firuza' and impl == 'opencl00':
+        return 'clSPARSE'
+    
+    return {
+        'mkl': 'MKL',
+        'gpu': 'cuSPARSE',
+        'sparsex': 'SparseX',
+        'native': 'Native'
+    }[impl]
+
+# Similarly, pretty-print the name of each benchmark / application
+def bench_map(bench):
+    return {
+        'pfold': 'Wales PFold',
+        'ngt': 'Wales NGT',
+        'PageRank': 'PageRank',
+        'bfs': 'BFS'
+    }[bench]
+
+# ... and the platform
+def platform_map(platform):
+    return {
+        'monaco': 'Intel-0',
+        'firuza': 'Intel-1',
+        'michel': 'AMD'
+    }[platform]
+
+def get_data(infile):
+    return pd.read_csv(infile)
+
+def plot(df, benches):
+    platforms = df.platform.unique()
+
+    fig, axes = plt.subplots(1, len(benches), sharey=True, figsize=fig_size(2.1, 0.67))
+
+    for plot_num, (bench, ax) in enumerate(zip(benches, axes)):
+        i = 0
+        
+        if plot_num == 0:
+            ax.set_ylabel('Speedup (×)')
+        
+        results = df.query("benchmark=='{}'".format(bench))
+        
+        ticks = []
+        labs = []
+        bars = []
+        legend = []
+        
+        rows = [r for r in results.iterrows()]
+        rows.sort(key=lambda r: platform_map(r[1].platform))
+        rows.reverse()
+
+        for row in rows:
+            y_val = row[1].speedup
+            bars.append(ax.bar(i, y_val, width=0.95))
+            ticks.append(i-0.2)
+            legend.append(platform_map(row[1].platform))
+            labs.append(impl_map(row[1].platform, row[1].implementation))
+            
+            valign = 'top' if y_val > 2.5 else 'bottom'
+            offset = -0.1 if y_val > 2.5 else 0.1
+            color = 'white' if y_val > 2.5 else 'black'
+            ax.text(i, y_val + offset, impl_map(row[1].platform,
+                row[1].implementation), rotation=90, verticalalignment=valign,
+                horizontalalignment='center', color=color, fontsize=8)
+            i += 1
+                    
+        ax.set_xticks([])
+        ax.set_xticklabels([])
+        ax.xaxis.set_tick_params(rotation=-45)
+
+        ax.set_ylim(0.8, 3.0)
+        ax.set_xlim(-0.6, 2.6)
+        
+        ax.axhline(y=1, color='white', linestyle=':')
+        
+        ax.set_title(bench_map(bench))
+        
+    ax.legend(bars, legend, bbox_to_anchor=(1.5,0.5), loc='center')
+      
+    fig.tight_layout()
+    plt.subplots_adjust(wspace=0.2)
+
+    sns.despine(fig)
 
 if __name__ == "__main__":
-    pass
+    parser = argparse.ArgumentParser(description='Baseline comparison plots for ICS')
+    parser.add_argument('data', type=str, help='Data file to tidy')
+    #parser.add_argument('-o', '--output', type=str, help='Output file to write')
+    args = parser.parse_args()
+
+    plot(get_data(args.data), ['pfold', 'ngt', 'PageRank', 'bfs'])
+    plt.savefig('out.pdf')
