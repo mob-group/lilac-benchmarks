e33a15d513d175251a39035eb3094432646e66e8
Bruce Collie <brucecollie82@gmail.com>
2019-01-28 11:51:40 +0000

    Split up pathsample data during tidying

--- results/ics/tidy.py
+++ results/ics/tidy.py
@@ -16,13 +16,31 @@ def tidy(filename):
     })
     return df.dropna()
 
+def new_benchmark(row):
+    return row['name'].split('.')[0]
+    
+def new_name(row):
+    return '.'.join(row['name'].split('.')[1:])
+
+def new_row(row):
+    if row['benchmark'] == 'PATHSAMPLE':
+        return [new_benchmark(row), new_name(row)]
+    else:
+        return row
+
+def split_pathsample(df):
+    df2 = df.copy()
+    df2[['benchmark', 'name']] = (
+        df2[['benchmark', 'name']].apply(new_row, axis=1, result_type='expand'))
+    return df2
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Data tidying script for ICS results.')
     parser.add_argument('data', type=str, help='Data file to tidy')
     parser.add_argument('-o', '--output', type=str, help='Output file to write')
     args = parser.parse_args()
 
-    tidied = tidy(args.data)
+    tidied = split_pathsample(tidy(args.data))
 
     if args.output is None:
         print(tidied.to_csv(index=False))
