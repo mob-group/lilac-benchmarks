90a52839869612ce17ae079ce7737de6aaa513cd
Bruce Collie <brucecollie82@gmail.com>
2019-01-29 17:27:57 +0000

    Consolidate data

--- results/ics/analysis.py
+++ results/ics/analysis.py
@@ -4,11 +4,14 @@ import argparse
 import pandas as pd
 import scipy.stats.mstats as mstats
 
-def get_speedups(df):
+def get_speedups(df, expert=False):
     native_times = (
         df.query('implementation == "native"')
           .groupby(['benchmark','platform', 'name'])
           .time.mean())
+
+    if not expert:
+        df = df[~df.implementation.str.contains('expert')]
 
     all_times = (
         df.groupby(['benchmark', 'platform', 'name', 'implementation'])
@@ -35,17 +38,59 @@ def best_per_benchmark(speedups):
 
     return mean_speedups.loc[max_mean_speedups]
 
+# From a group of mean speedups by benchmark, platform, implementation, get the
+# speedup for a particular implementation.
+def speedup_for_impl(impl, group):
+    for r in group.iterrows():
+        if r[1]['implementation'] == impl:
+            return r[1]['speedup']
+    return float('nan')
+
+# Get the performance ratio
+def speedup_pairs(g):
+    targets = [
+        ('gpu', 'opencl-expert'),
+        ('mkl', 'openmp-expert')
+    ]
+
+    return pd.Series(
+        [speedup_for_impl(t[0], g) / speedup_for_impl(t[1], g) for t in targets],
+        index=[t[1] for t in targets]
+    )
+
+def best_vs_expert(speedups):
+    mean_speedups = (
+        speedups.groupby(['benchmark', 'platform', 'implementation'])['speedup']
+                .apply(mstats.gmean)
+                .reset_index())
+
+    return (
+        mean_speedups.groupby(['benchmark', 'platform'])
+                     .apply(lambda g: speedup_pairs(g))
+                     .dropna()
+                     .reset_index()
+    )
+
+modes = {
+    'baseline' : best_per_benchmark,
+    'expert' : best_vs_expert
+}
+
+def print_results(df, out=None):
+    if out is None:
+        print(df.to_csv(index=False))
+    else:
+        df.to_csv(out, index=False)
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Data analysis script for ICS results.')
+    parser.add_argument('mode', choices=modes, help='Mode to use')
     parser.add_argument('data', type=str, help='Data file to analyse')
     parser.add_argument('-o', '--output', type=str, help='Output file to write')
     args = parser.parse_args()
 
     df = pd.read_csv(args.data)
-    speeds = get_speedups(df)
-    bests = best_per_benchmark(speeds)
+    speeds = get_speedups(df, args.mode=='expert')
+    bests = modes[args.mode](speeds)
 
-    if args.output is None:
-        print(bests.to_csv(index=False))
-    else:
-        bests.to_csv(args.output, index=False)
+    print_results(bests, args.output)
